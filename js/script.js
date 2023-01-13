654cc30e3e8a62bf198923212ba6b999f33f4cb4
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-13 16:04:51 -0300

    hideTabContent function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorALL('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+
+    }
+
+
+});

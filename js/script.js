09fa8d911d139cd43bc8c8caf73e1c6a7a23680e
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-15 16:34:49 -0300

    Timer initial commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
+    
     const tabs = document.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent'),
             tabsParent = document.querySelector('.tabheader__items');
@@ -38,6 +40,29 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const deadline = '2023-01-29';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()), 
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60 % 60)),
+            seconds = Math.floor((t / 1000 % 60));
+
+        return {
+            'total': t, 
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    // функция, которая будет заниматься обновлением таймера
 
 
 

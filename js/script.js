9e926ba618557e12ddc93facb3880dbc0a129da0
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-18 18:35:46 -0300

    function getZero

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
             hours = Math.floor((t / (1000 * 60 * 60) % 24)),
             minutes = Math.floor((t / 1000 / 60 % 60)),
-            seconds = Math.floor((t / 1000 % 60));
+            seconds = Math.floor((t / 1000) % 60);
 
         return {
             'total': t, 
@@ -60,6 +60,14 @@ document.addEventListener('DOMContentLoaded', () => {
             'seconds': seconds
         };
     }   
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
     
     function setClock(selector, endtime) {
             const timer = document.querySelector(selector),
@@ -68,20 +76,23 @@ document.addEventListener('DOMContentLoaded', () => {
                     minutes = timer.querySelector('#minutes'),
                     seconds = timer.querySelector('#seconds'),
                     timeInterval = setInterval(updateClock, 1000);
-            
+
+            updateClock();
+             
             function updateClock() {
                 const t = getTimeRemaining(endtime); 
 
-                days.innerHTML = t.days;
-                days.innerHTML = t.hours;
-                days.innerHTML = t.minutes;
-                days.innerHTML = t.seconds;
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
 
                 if (t.total <=0) {
                     clearInterval(timeInterval);
                 }                 
             }   
-    }  
+    } 
 
+    setClock('.timer', deadline);
 
 });

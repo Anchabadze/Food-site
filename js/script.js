a13e2b565132f2e495fe67632542e6110cbde9a6
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-23 17:26:17 -0300

    reset and timeout

--- js/script.js
+++ js/script.js
@@ -248,6 +248,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (request.status === 200) { 
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 3000);
                 } else { 
                     statusMessage.textContent = message.failure; 
                 }

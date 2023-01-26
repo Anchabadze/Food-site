04dfb236208522613df581898812efbf8e201b9f
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-26 13:32:35 -0300

    slider dots

--- js/script.js
+++ js/script.js
@@ -299,7 +299,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Slider
 
-    const slides = document.querySelectorAll('.offer__slide'), 
+    const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'), 
         next = document.querySelector('.offer__slider-next'), 
         total = document.querySelector('#total'), 
@@ -328,6 +329,55 @@ document.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width; 
     });
 
+    slider.style.position = 'relative';
+
+    const dots = document.createElement('ol'),
+            massDots = [];
+
+    dots.classList.add('carousel-dots');
+    // dots.style.cssText = `
+    //     position: absolute;
+    //     right: 0;
+    //     bottom: 0;
+    //     left: 0;
+    //     z-index: 15;
+    //     display: flex;
+    //     justify-content: center;
+    //     margin-right: 15%;
+    //     margin-left: 15%;
+    //     list-style: none;
+    // `;
+    slider.append(dots);
+
+    for (let i = 0; i < slides.length; i++ ) { 
+        const dot = document.createElement('li'); 
+        dot.setAttribute('data-slide-to', i + 1); 
+        dot.classList.add('dot');
+        // dot.style.cssText = `
+        //     box-sizing: content-box;
+        //     flex: 0 1 auto;
+        //     width: 30px;
+        //     height: 6px;
+        //     margin-right: 3px;
+        //     margin-left: 3px;
+        //     cursor: pointer;
+        //     background-color: #fff;
+        //     background-clip: padding-box;
+        //     border-top: 10px solid transparent;
+        //     border-bottom: 10px solid transparent;
+        //     opacity: .5;
+        //     transition: opacity .6s ease;
+        // `;
+
+        if (i == 0) { // если индекс 0 (первая интерация)
+            dot.style.opacity = 1; // то прозрачность 1
+        }
+
+        dots.append(dot); 
+        massDots.push(dot);
+    }
+
+
     next.addEventListener('click', () => { 
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { 
             offset = 0; 
@@ -348,6 +398,9 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        massDots.forEach(dot => dot.style.opacity ='0.5');
+        massDots[slideIndex - 1].style.opacity = '1'; 
 
 
     });
@@ -372,6 +425,10 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        massDots.forEach(dot => dot.style.opacity ='0.5'); 
+        massDots[slideIndex - 1].style.opacity = '1';
+
     });
 
     // showSlides(slideIndex); 

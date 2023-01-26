5b68ba30620d2ee3f359023000f1feda35cb541d
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-26 14:34:56 -0300

    slider dots click

--- js/script.js
+++ js/script.js
@@ -429,6 +429,28 @@ document.addEventListener('DOMContentLoaded', () => {
         massDots.forEach(dot => dot.style.opacity ='0.5'); 
         massDots[slideIndex - 1].style.opacity = '1';
 
+        massDots.forEach(dot => { 
+            dot.addEventListener('click', (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to');
+
+                slideIndex = slideTo;
+
+                offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+                slidesField.style.transform = `translateX(-${offset}px)`; 
+
+                if (slides.length < 10) {
+                    current.textContent = `0${slideIndex}`;
+                } else {
+                    current.textContent = slideIndex;
+                }
+
+                massDots.forEach(dot => dot.style.opacity ='0.5'); 
+                massDots[slideIndex - 1].style.opacity = '1';
+
+            });
+        });
+
     });
 
     // showSlides(slideIndex); 

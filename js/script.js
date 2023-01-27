586c63a1814fe7e9776c186149671ab00478d5c3
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-27 11:36:30 -0300

    calc func dynamic data

--- js/script.js
+++ js/script.js
@@ -503,42 +503,67 @@ document.addEventListener('DOMContentLoaded', () => {
     let sex, height, weight, age, ratio;
 
     function calcTotal() {
-        if (!sex || !height || !weight || !age || !ratio) { // если у нас нет значения пола, роста, веса ....
-            result.textContent = '_____'; // выводится в строке с результатом
-            return; // чтобы досрочно прервать функцию
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '_____';
+            return;
         } 
 
-        if (sex === 'female') { // если пол будет женский
-            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
-        } else { // в другом случае (если мужской)
-            result.textContent = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) * ratio;
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) * ratio);
         }
     }
 
-    calcTotal(); // сразу вызываем функцию
+    calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) { // создаём функцию по получения статичных данных со страницы с атрибутами (родительский элемент, класс активности)
-        const elements = document.querySelectorAll(`${parentSelector} div`); // получаем все элементы внутри родтельского элемента
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
 
         document.querySelector(parentSelector).addEventListener('click', (e) => {
-            if (e.target.getAttribute('data-ratio')) { // если у объекта события есть атрибут data-ratio
-                ratio = +e.target.getAttribute('data-ratio')// мы устанавливаем переменную ratio в значение, которое есть у элемента в data
-            } else { // если у объекта события нет data атрибута (то есть есть индивид. индетификатор)
-                sex = e.target.getAttribute('id'); // значение sex становится равным id
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio')
+            } else {
+                sex = e.target.getAttribute('id');
             }
 
-            console.log(ratio, sex);
-
-            elements.forEach(elem => { // перебираем все элементы внутри родительского класса
-                elem.classList.remove(activeClass); // убираем у всех элементов класс .activeClass
+            elements.forEach(elem => {
+                elem.classList.remove(activeClass);
             });
     
-            e.target.classList.add(activeClass); // тому div, в который мы кликнули, назначаем класс активности
+            e.target.classList.add(activeClass);
+
+            calcTotal();
         });
     }
 
-    getStaticInformation('#gender', '.calculating__choose-item_active'); // вызываем функцию для пола
-    getStaticInformation('.calculating__choose_big', '.calculating__choose-item_active'); // вызываем функцию для физичекой активности
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamocInformation(selector) {
+        const input = document.querySelector(selector); 
+
+        input.addEventListener('input', () => { 
+            switch(input.getAttribute('id')) { 
+                case 'height':
+                    height = +input.value; 
+                    break; 
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+
+    }
+
+    getDynamocInformation('#height');
+    getDynamocInformation('#weight');
+    getDynamocInformation('#age');
 
 
 
